1a83416005a4d76324ff6dc16c10edaf10ccb63c
arcatica <arcatica@6f236aed-a213-c02b-bf4c-6b3998597d33>
2010-04-05 20:26:13 +0000

    Initial Commit

---
+++ Kurssi.java
@@ -0,0 +1,14 @@
+
+public class Kurssi {
+
+	private String nimi;
+	private int laajuus;
+	private boolean suoritettu;
+	
+	
+	public Object getNimi() {
+		// TODO Auto-generated method stub
+		return this.nimi;
+	}
+
+}
