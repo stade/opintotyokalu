aec7e3fb6b02538039fd8ce33e97b599e5d31502
juho.hame <juho.hame@6f236aed-a213-c02b-bf4c-6b3998597d33>
2010-04-06 12:27:38 +0000

    Lisäsin konstruktorin ja getterit

--- Kurssi.java
+++ Kurssi.java
@@ -4,11 +4,27 @@ public class Kurssi {
 	private String nimi;
 	private int laajuus;
 	private boolean suoritettu;
-	
-	
-	public Object getNimi() {
-		// TODO Auto-generated method stub
+
+	public Kurssi(String nimi, int laajuus) {
+		this.nimi = nimi;
+		this.laajuus = laajuus;
+		this.suoritettu = false;	// oletus: ei suoritettu (?)
+	}
+
+	public void setSuoritettu(boolean onko) {
+		this.suoritettu = onko;
+	}
+
+	public String getNimi() {
 		return this.nimi;
+	}
+
+	public int getLaajuus() {
+		return this.laajuus;
+	}
+
+	public int getSuoritettu() {
+		return this.suoritettu;
 	}
 
 }
