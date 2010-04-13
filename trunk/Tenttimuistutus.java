535e5d2d1c9d53092615409b1c5bd41ea6fe0a3d
tatu.tahvanainen <tatu.tahvanainen@6f236aed-a213-c02b-bf4c-6b3998597d33>
2010-04-13 11:14:21 +0000

    Tulostaa nyt huomatuksen jos tulevia tenttejä ei ole

--- trunk/Tenttimuistutus.java
+++ trunk/Tenttimuistutus.java
@@ -34,6 +34,8 @@ public class Tenttimuistutus {
 
 		//Järjestelmän tämän hetkinen aika.
 		Date aika = new Date();
+		
+		
 
 		//Kopioidaan tentit väliaikaiseen tietorakenteeseen
 		for (int i = 0; i < tentit.size(); i++ ) {
@@ -42,67 +44,78 @@ public class Tenttimuistutus {
 			if (tentit.get(i).getAlku().after(aika)) {
 				temp.add(tentit.get(i));
 			}
-
+			
 
 		}
-
+		
 		Scanner nappaimisto;
-		Kayttoliittyma.tyhjennaNakyma();
-		tulostaVali(10);
-		System.out.print("Huom!");
-
-		tulostaVali(10);
-		rivinvaihto(2);
-		tulostaVali(5);
-
-		System.out.print("Tulevat tentit:");
-		tulostaVali(5);
-		rivinvaihto(2);
-
-
-		/*
-		 *	Tulostaa tulevat tentit aikajärjestyksessä niin, että etsii lähimpänä olevan
-		 *	tentin poistaa sen tietorakenteesta ja tulostaa sen. Tätä jatketaan kunnes
-		 *	tietorakenne on tyhjä.
-		 */
-		while (temp.isEmpty() == false) {
-
-			lahinna = temp.get(0);
-
-			if (indeksi >= temp.size()) {
-				break;
-			}
-
-			//Käydään kaikki tentit läpi ja etsitään tentti johon on vähiten aikaa
-			for (int i = 0; i < temp.size(); i++ ) {
-
-				tentti = temp.get(i);
-
-				//Jos löytyi tentti joka on lähempänä kuin edellinen lähin korvataan se.
-				if (tentti.getAlku().after(lahinna.getAlku())) {
-					lahinna = tentti;
-					indeksi = i;
-				}
-			}
-
-			//Poistetaan lähinnä oleva tentti väliaikaisesta tietorakenteesta
-			temp.remove(indeksi);
-			tulostaVali(1);
-
-			//Tulostetaan kurssi johon tentti kuuluu
-			System.out.print(lahinna.getKuuluuKurssiinNimelta());
-			tulostaVali(1);
-
-			//Tulostetaan tentin sijainti
-			System.out.print(lahinna.getSijainti());
-			tulostaVali(1);
-
-			//Tulostetaan tentin alkamisaika muodossa "dd.MM. hh"
-			DateFormat dateFormat = new SimpleDateFormat("dd.MM. 'klo' hh");
-			System.out.print(dateFormat.format(lahinna.getAlku()));
-			rivinvaihto(1);
+		
+		if (temp.size() == 0) {
+			
+			Kayttoliittyma.tyhjennaNakyma();
+			tulostaVali(5);
+			System.out.println("Ei tulevia tenttejä");
+			tulostaVali(5);
 		}
+		else {
 
+			
+			Kayttoliittyma.tyhjennaNakyma();
+			tulostaVali(10);
+			System.out.print("Huom!");
+
+			tulostaVali(10);
+			rivinvaihto(2);
+			tulostaVali(5);
+
+			System.out.print("Tulevat tentit:");
+			tulostaVali(5);
+			rivinvaihto(2);
+
+
+			/*
+			 *	Tulostaa tulevat tentit aikajärjestyksessä niin, että etsii lähimpänä olevan
+			 *	tentin poistaa sen tietorakenteesta ja tulostaa sen. Tätä jatketaan kunnes
+			 *	tietorakenne on tyhjä.
+			 */
+			while (temp.isEmpty() == false) {
+
+				lahinna = temp.get(0);
+
+				if (indeksi >= temp.size()) {
+					break;
+				}
+
+				//Käydään kaikki tentit läpi ja etsitään tentti johon on vähiten aikaa
+				for (int i = 0; i < temp.size(); i++ ) {
+
+					tentti = temp.get(i);
+
+					//Jos löytyi tentti joka on lähempänä kuin edellinen lähin korvataan se.
+					if (tentti.getAlku().after(lahinna.getAlku())) {
+						lahinna = tentti;
+						indeksi = i;
+					}
+				}
+
+				//Poistetaan lähinnä oleva tentti väliaikaisesta tietorakenteesta
+				temp.remove(indeksi);
+				tulostaVali(1);
+
+				//Tulostetaan kurssi johon tentti kuuluu
+				System.out.print(lahinna.getKuuluuKurssiinNimelta());
+				tulostaVali(1);
+
+				//Tulostetaan tentin sijainti
+				System.out.print(lahinna.getSijainti());
+				tulostaVali(1);
+
+				//Tulostetaan tentin alkamisaika muodossa "dd.MM. hh"
+				DateFormat dateFormat = new SimpleDateFormat("dd.MM. 'klo' hh");
+				System.out.print(dateFormat.format(lahinna.getAlku()));
+				rivinvaihto(1);
+			}
+		}
 		rivinvaihto(1);
 		tulostaVali(3);
 		System.out.print("Paina enter jatkaaksesi!");
